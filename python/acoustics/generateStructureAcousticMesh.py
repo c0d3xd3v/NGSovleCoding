514c24d223cc2ae5e1324ef26b1e902740b9864b
Kai Henning <khenning@math.tu-berlin.de>
2023-03-30 00:53:52 +0200

    * delete unneeded test_data * add poission to test solving pde on air domain * need to set inhomogenous dirichlet in inner boundary (solid)

--- python/acoustics/generateStructureAcousticMesh.py
+++ python/acoustics/generateStructureAcousticMesh.py
@@ -35,13 +35,33 @@ mesh.GenerateVolumeMesh()
 ngmesh = ngsolve.Mesh(mesh)
 
 _fes: VectorH1 = VectorH1(ngmesh, definedon="solid", order=2, complex=True)
-a, b = build_elasticity_system_on_fes(ngmesh, steel, _fes, 10)
+a, b = build_elasticity_system_on_fes(ngmesh, steel, _fes, 20)
 u = ngsolve.GridFunction(_fes, multidim=20)
-lams = ngsolve.ArnoldiSolver(a.mat, b.mat, _fes.FreeDofs(), list(u.vecs), 10)
+lams = ngsolve.ArnoldiSolver(a.mat, b.mat, _fes.FreeDofs(), list(u.vecs), 20)
 
-fes2 = H1(ngmesh, definedon="air")
-u2 = GridFunction(fes2, "u2")
-u2.Set(0)
+fes2 = H1(ngmesh, dirichlet=[1,2], definedon="air")
+#u2 = GridFunction(fes2, "u2")
+#u2.Set(0)
+
+
+# define trial- and test-functions
+u2 = fes2.TrialFunction()
+v2 = fes2.TestFunction()
+
+# the right hand side
+f2 = LinearForm(fes2)
+f2 += SymbolicLFI(1 * v2)
+
+# the bilinear-form 
+a2 = BilinearForm(fes2, symmetric=True)
+a2 += SymbolicBFI(grad(u2)*grad(v2))
+
+a2.Assemble()
+f2.Assemble()
+
+# the solution field 
+gfu2 = GridFunction(fes2)
+gfu2.vec.data = a2.mat.Inverse(fes2.FreeDofs(), inverse="sparsecholesky") * f2.vec
 
 fes3 = H1(ngmesh, definedon="pml")
 u3 = GridFunction(fes3, "u3")
@@ -49,6 +69,7 @@ u3.Set(1)
 
 
 Draw(ngmesh)
-Draw(u)
-Draw(u2)
-Draw(u3)
+#Draw(u)
+#Draw(u2)
+#Draw(u3)
+Draw(gfu2)
